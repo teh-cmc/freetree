db9cefd008a4fc520c2466260bcfa2304f52cc50
cmc <cr.rey.clement@gmail.com>
2015-11-27 16:51:26 +0100

    TestFreeTree_pair_unsorted_input_rebalanced

--- freetree_test.go
+++ freetree_test.go
@@ -126,41 +126,43 @@ func TestFreeTree_pair_unsorted_input(t *testing.T) {
 	}
 }
 
-//func TestFreeTree_pair_unsorted_input_rebalanced(t *testing.T) {
-//expected := ComparableArray{intTest(1), intTest(3), intTest(2), intTest(5), intTest(6), intTest(4)}
+func TestFreeTree_pair_unsorted_input_rebalanced(t *testing.T) {
+	expected := ComparableArray{intTest(1), intTest(3), intTest(2), intTest(5), intTest(6), intTest(4)}
 
-//st := NewFreeTree()
-//cs := ComparableArray{intTest(5), intTest(4), intTest(6), intTest(1), intTest(3), intTest(2)}
+	st := NewSimpleTree()
+	cs := ComparableArray{intTest(5), intTest(4), intTest(6), intTest(1), intTest(3), intTest(2)}
 
-//st.InsertArray(cs)
-//st.Rebalance()
-//flat := st.Flatten()
+	st.InsertArray(cs)
 
-//for i := range flat {
-//if flat[i] != expected[i] {
-//t.Error("expected != flat")
-//t.FailNow()
-//}
-//}
+	ft, err := NewFreeTree(st.Rebalance())
+	if err != nil {
+		t.Error(err)
+	}
+	flat := ft.Flatten()
 
-//if st.Ascend(intTest(1)).(intTest) != intTest(1) {
-//t.Error("unexpected retval")
-//}
-//if st.Ascend(intTest(2)).(intTest) != intTest(2) {
-//t.Error("unexpected retval")
-//}
-//if st.Ascend(intTest(5)).(intTest) != intTest(5) {
-//t.Error("unexpected retval")
-//}
-//if st.Ascend(intTest(6)).(intTest) != intTest(6) {
-//t.Error("unexpected retval")
-//}
-//if st.Ascend(intTest(7)) != nil {
-//log.Println(st.Ascend(intTest(7)).(intTest))
-//t.Error("unexpected retval")
-//}
-//if st.Ascend(intTest(8)) != nil {
-//log.Println(st.Ascend(intTest(8)).(intTest))
-//t.Error("unexpected retval")
-//}
-//}
+	for i := range flat {
+		if flat[i] != expected[i] {
+			t.Error("expected != flat")
+			t.FailNow()
+		}
+	}
+
+	if ft.Ascend(intTest(1)).(intTest) != intTest(1) {
+		t.Error("unexpected retval")
+	}
+	if ft.Ascend(intTest(2)).(intTest) != intTest(2) {
+		t.Error("unexpected retval")
+	}
+	if ft.Ascend(intTest(5)).(intTest) != intTest(5) {
+		t.Error("unexpected retval")
+	}
+	if ft.Ascend(intTest(6)).(intTest) != intTest(6) {
+		t.Error("unexpected retval")
+	}
+	if ft.Ascend(intTest(7)) != nil {
+		t.Error("unexpected retval")
+	}
+	if ft.Ascend(intTest(8)) != nil {
+		t.Error("unexpected retval")
+	}
+}
